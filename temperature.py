0db00d06a0fa93b96e52bd321737476ea3282137
Alejandro <alejandro.gonzalez3@udc.es>
2021-05-28 17:17:26 +0200

    [TEMPERATURE] Fix temperature Script

--- temperature.py
+++ temperature.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
 import time
-from w1thermsensor import W1ThermSensor
+# from w1thermsensor import W1ThermSensor
 
 import requests
+import json
 
-sensor = W1ThermSensor()
+# sensor = W1ThermSensor()
 
-authenticateUrl = ""
-temperatureUrl = ""
+baseUrl = "http://localhost:8080"
+
+authenticateUrl = baseUrl + "/user/login"
+temperatureUrl = baseUrl + "/sensorValue/store"
+
+username = "string"
+password = "string"
 
 while True:
     try:
-        temperature = sensor.get_temperature()
-        print("Temperatura: %s centigrados" % temperature)
-        # Meter Token de autenticacion
-        # authenticate = requests.post(authenticateUrl, data = {'username':username, 'password':password})
-        # r = requests.post(temperatureUrl, data = {'value':temperature}, headers={"content-type":"json", "apiToken":apiToken})
+        # Sensor value
+        # temperature = sensor.get_temperature()
+
+        # For testing
+        temperature = 14.58
         
-        # r = requests.post(temperatureUrl, data = {'value':temperature})
+        print("Temperatura: %s centigrados" % temperature)
+
+        # authenticate = requests.post(authenticateUrl, params = {'username':username, 'password':password})
+        # print("Token: %s" % authenticate.text)
+        # r = requests.post(temperatureUrl, data = {'value':temperature}, headers={"content-type":"json", "apiToken":apiToken})
+        r = requests.post(temperatureUrl, params = {'sensorName':'temperatura', 'value':temperature})
+        print("Temperature POST response: %s" % r.status_code)
         time.sleep(1)
     except requests.exceptions.HTTPError as e:
         time.sleep(10)

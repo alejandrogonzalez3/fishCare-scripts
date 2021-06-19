5b69334cce33e4d408e024272a7bbee0a40be981
alejandro.gonzalez3 <alejandro.gonzalez3@udc.es>
2021-06-19 16:40:08 +0200

    [ARDUINO/MQTT_RELAY] Add GPIO logic to MQTT_RELAY script and improve Arduino.py

--- arduino.py
+++ arduino.py
@@ -8,7 +8,7 @@ arduino = serial.Serial('/dev/ttyACM0', 115200)
 baseUrl = "http://localhost:8080"
 
 authenticateUrl = baseUrl + "/user/login"
-temperatureUrl = baseUrl + "/sensorValue/store"
+sensorUrl = baseUrl + "/sensorValue/store"
 
 username = "string"
 password = "string"
@@ -24,14 +24,19 @@ while True:
             line = arduino.readline().decode('utf-8').rstrip()
             # print(line)
             if "ppm" in line:
-                  # r = requests.post(temperatureUrl, data = {'value':tds}, headers={"content-type":"json", "apiToken":apiToken})
-                  # r = requests.post(temperatureUrl, params = {'sensorName':'tds', 'value':line})
+                  # r = requests.post(sensorUrl, params = {'sensorName':'tds', 'value':line}, headers={"content-type":"json", "apiToken":apiToken})
+                  # r = requests.post(sensorUrl, params = {'sensorName':'tds', 'value':line})
                   # print("TDS POST response: %s" % r.status_code)
                   print("TDS Value:", line)
             if "pH" in line:
-                  # r = requests.post(temperatureUrl, data = {'value':pH}, headers={"content-type":"json", "apiToken":apiToken})
-                  # r = requests.post(temperatureUrl, params = {'sensorName':'pH', 'value':line})
+                  # r = requests.post(sensorUrl, params = {'sensorName':'pH', 'value':line}, headers={"content-type":"json", "apiToken":apiToken})
+                  # r = requests.post(sensorUrl, params = {'sensorName':'pH', 'value':line})
                   # print("pH POST response: %s" % r.status_code)
                   print("pH Value:", line)
+            if "ms/cm" in line:
+                  # r = requests.post(sensorUrl, params = {'sensorName':'conductivity', 'value':line}, headers={"content-type":"json", "apiToken":apiToken})
+                  # r = requests.post(sensorUrl, params = {'sensorName':'conductivity', 'value':line})
+                  # print("conductivity POST response: %s" % r.status_code)
+                  print("conductivity Value:", line)
 
 arduino.close() #Finalizamos la comunicacion
--- mqtt_relay.py
+++ mqtt_relay.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
+
+# Notas de autor: 
+# 1.- O Relé ten que estar a 3.3v para funcionar.
+# 2.- Os pins están en BCM, polo tanto hai que conectar ao GPIO 17, pin 11 (6º comezando dende arriba, fila da esquerda)
+
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+RELAY_1_GPIO = 17
 
 # The callback for when the client connects to the broker
 def on_connect(client, userdata, flags, rc):
-    # Print result of connection attempt
     print("Connected with result code {0}".format(str(rc)))
     # Subscribe to the topic
-    client.subscribe("raspberry_relay/oxigenator")
+    client.subscribe("raspberry_relay/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print("Message received-> " + msg.topic + " " + str(msg.payload.decode()))
+    decodedString = str(msg.payload.decode())
+    print("Message received-> " + msg.topic + " " + decodedString)
+    if "oxigenator" in msg.topic:
+        if "on" in decodedString:
+            GPIO.output(RELAY_1_GPIO, GPIO.LOW)
+        elif "off" in decodedString:
+            GPIO.output(RELAY_1_GPIO, GPIO.HIGH)
 
-client = mqtt.Client()  # Create instance of client with client ID “digi_mqtt_test”
-client.on_connect = on_connect  # Define callback function for successful connection
-client.on_message = on_message  # Define callback function for receipt of a message
+try:
+    GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+    GPIO.setup(RELAY_1_GPIO, GPIO.OUT, initial = GPIO.HIGH) # GPIO Assign mode
 
-# Connect to (broker, port, keepalive-time)
-client.connect("broker.mqttdashboard.com", 1883, 60)
+    client = mqtt.Client()
+    client.on_connect = on_connect  # Define callback function for successful connection
+    client.on_message = on_message  # Define callback function for receipt of a message
 
-client.loop_forever()  # Start networking daemon
+    # Connect to (broker, port, keepalive-time)
+    client.connect("broker.mqttdashboard.com", 1883, 60)
+
+    client.loop_forever()  # Start networking daemon
+    
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+   print("\nKeyboard interrupt")
+
+finally:
+   print("clean up GPIO pins") 
+   GPIO.cleanup()

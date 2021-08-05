e20dc2ca3f9278e0cf8841a44e0ad6fd67b25f25
alejandro.gonzalez3 <alejandro.gonzalez3@udc.es>
2021-08-05 02:42:27 +0200

    [Temperature / flow_meter] Improve flow metter script and restore temperature script default behaviour

--- temperature.py
+++ temperature.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import time
-# from w1thermsensor import W1ThermSensor
+from w1thermsensor import W1ThermSensor
 
 import requests
 import json
 
-# sensor = W1ThermSensor()
+sensor = W1ThermSensor()
 
 baseUrl = "http://localhost:8080"
 
@@ -18,19 +18,19 @@ password = "string"
 while True:
     try:
         # Sensor value
-        # temperature = sensor.get_temperature()
+        temperature = sensor.get_temperature()
 
         # For testing
-        temperature = 14.58
-        
+        #temperature = 14.58
+
         print("Temperatura: %s centigrados" % temperature)
 
         # authenticate = requests.post(authenticateUrl, params = {'username':username, 'password':password})
         # print("Token: %s" % authenticate.text)
         # r = requests.post(temperatureUrl, data = {'value':temperature}, headers={"content-type":"json", "apiToken":apiToken})
-        r = requests.post(temperatureUrl, params = {'sensorName':'temperatura', 'value':temperature})
-        print("Temperature POST response: %s" % r.status_code)
+        #r = requests.post(temperatureUrl, params = {'sensorName':'temperatura', 'value':temperature})
+        #print("Temperature POST response: %s" % r.status_code)
         time.sleep(1)
     except requests.exceptions.HTTPError as e:
         time.sleep(10)
-    
+
